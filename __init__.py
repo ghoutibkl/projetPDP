c06fef1ac7023144cba2d92d5c57f27826940803
venire-ute <magaye.ndiaye@etu.u-bordeaux.fr>
2024-01-29 13:32:04 +0100

    adding model_1

---
+++ __init__.py
@@ -0,0 +1,20 @@
+
+from models import model_1
+import click
+@click.command()
+@click.argument('model', type=click.Choice(['model_1','linear', 'logistic']), default='model_1',)
+# @click.option('-n','--number',  type=int, help="please enter the number of time you want to show the message")
+def main(model):
+    '''
+     help message 
+    '''
+
+    if model is None:
+        model = input('please enter your name: ')
+    if model == 'model_1':
+        click.echo('model_1 ')
+        print(model_1(1,2,3,4))
+
+
+if __name__ == "__main__":
+    main()
---
+++ models/utils.py
@@ -0,0 +1,5 @@
+def get_port_average_water_level(port):
+    """
+        Return the average water level of a port.
+    """
+    return 10
